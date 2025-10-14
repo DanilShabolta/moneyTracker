0d4faa3e7b82e4fdc3f4ee488c1e541a066a3d79
danilshabolta <danilshabolta@gmail.com>
2025-10-14 18:10:47 +0700

    рандом

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.compose")
@@ -8,40 +8,41 @@ plugins {
 
 android {
     namespace = "com.example.moneytracker"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.moneytracker"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     buildFeatures {
         compose = true
     }
     composeOptions {
-        // ИСПРАВЛЕНО: Версия компилятора Compose для Kotlin 2.0.0+
-        kotlinCompilerExtensionVersion = "1.5.12"
+        kotlinCompilerExtensionVersion = "1.7.4"
     }
     packaging {
         resources {
@@ -51,45 +52,37 @@ android {
 }
 
 dependencies {
-    // ----------------------------------------------------
-    //  КОМПОНЕНТЫ ANDROID И COMPOSE
-    // ----------------------------------------------------
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-
-    // Compose & Material
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0") // Material 3
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-
-    // ----------------------------------------------------
-    //  DAGGER HILT (DI)
-    // ----------------------------------------------------
-    implementation("com.google.dagger:hilt-android:2.50")
-    ksp("com.google.dagger:hilt-compiler:2.50")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-
-    // ----------------------------------------------------
-    //  ROOM (БАЗА ДАННЫХ)
-    // ----------------------------------------------------
     val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion") // Для Coroutines Flow
 
-    // ----------------------------------------------------
-    //  WORK MANAGER (УВЕДОМЛЕНИЯ)
-    // ----------------------------------------------------
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
-    implementation("androidx.hilt:hilt-work:1.1.0")
-    ksp("androidx.hilt:hilt-compiler:1.1.0")
-
-    // ----------------------------------------------------
-    //  ГРАФИКИ (Используем чистый Canvas)
-    // ----------------------------------------------------
-    // Сторонние библиотеки не используются
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.navigation.runtime.android)
+    implementation(libs.androidx.navigation.compose.android)
+    implementation(libs.androidx.foundation.android)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.core.ktx.vx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.material3)
+    ksp(libs.androidx.room.compiler.v261)
+    implementation(libs.androidx.room.ktx.v261)
+    implementation(libs.androidx.room.runtime.v261)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.hilt.android.v250)
+    ksp(libs.hilt.compiler.v250)
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.material3.v120)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.work.runtime.ktx.v290)
+    implementation(libs.androidx.hilt.work)
 }
